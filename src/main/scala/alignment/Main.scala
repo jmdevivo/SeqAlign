b73a40f18f5f460fa95e45668faf49a85ecc0eff
jmdevivo <jdevivo@gmail.com>
2015-04-27 15:32:11 -0400

    Created Jobs to distribute among nodes

--- src/main/scala/alignment/Main.scala
+++ src/main/scala/alignment/Main.scala
@@ -1,6 +1,8 @@
 package alignment
 
 
+import java.util
+
 import org.apache.ignite.cache.{CachePeekMode, CacheMode}
 import org.apache.ignite.cluster.ClusterGroup
 import org.apache.ignite.configuration.CacheConfiguration
@@ -9,11 +11,12 @@ import org.apache.ignite.scalar.scalar
 import org.apache.ignite.scalar.scalar._
 import org.apache.ignite._
 import scala.io.Source
+import scala.collection.JavaConversions._
 
 
 /**
  * Write a program that creates 11 thousand jobs. Each job aligns two sequences(this is a shitty way to do it)
- * (100 jobs 100 sequences each is better). Create an array of runnables where each runnable is one of these jobs
+ * (100 jobs 100 sequences each is gibetter). Create an array of runnables where each runnable is one of these jobs
  * Take sequence you read in from file align it with every one of the 11 thousond sequences. Check testing programs
  * inspiration on referencing sequences by key
  * Each program must take a sequence and a name, look up a sequence in the cache based on that name. Do an alignment
@@ -21,10 +24,27 @@ import scala.io.Source
  * Created by jmdevivo on 4/24/15.
  */
 object Main extends App{
+
   Ignition.setClientMode(true)
 
   scalar {
-    
+    val serverGroup: ClusterGroup = ignite$.cluster().forServers()
+
+    val serverCompute: IgniteCompute = ignite$.compute(serverGroup)
+
+    class RunnableJob(name: String) extends IgniteRunnable {
+      override def run = {
+        val clientCache: IgniteCache[String, String] = ignite$.cache[String, String]("16S_Sequences")
+        //println(clientCache.size(CachePeekMode.ALL))
+        println(s"$name\t${clientCache.get(name).length}")
+      }
+    }
+
+    val jobs: java.util.Collection[IgniteRunnable] = Source.fromFile("seqKeys.txt").getLines.toArray.map(new RunnableJob(_)).toSeq
+
+      //serverCompute.broadcast(compare)
+    serverCompute.run(jobs)
   }
+
 
 }
